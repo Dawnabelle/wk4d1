a6c4faddf4c59ca444b440c4fee41c684f4f665b
Cameron Button, Kelsey Guerra and Matt Nardoni <matt@>
2018-04-02 14:00:36 -0700

    finish places project

--- places/js/scripts.js
+++ places/js/scripts.js
@@ -1,23 +1,35 @@
-function locationTest (place, time) {
+// function Destination() {
+//   place = "";
+//   time = "";
+//   notes = "";
+//
+// }
+
+function locationTest (place, date, notes) {
   this.place = place;
-  this.time = time;
+  this.date = date;
+  this.notes = notes;
 }
 
-function makeCardBody (){
 
-}
+// var texas = new locationTest('Texas', 'spring 2018');
 
-var texas = new locationTest('Texas', 'spring 2018');
-
-locationTest.prototype.toString = function locationTestToString() {
-  return this.place + ' ' + this.time;
-}
+// locationTest.prototype.toString = function locationTestToString() {
+//   return this.place + ' ' + this.time +' '+ this.notes;
+// }
 
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
-    var userInput = new locationTest($("#place").val(), $("#time").val());
-    $("#places-list").append();
+    var userInput = new locationTest($("#place").val(), $("#date").val(), $('#notes').val());
+    $("#places-list").append(
+      '<div class="card">' +
+      '<div class="card-header bg-info">' + userInput.place +
+      '</div>' +
+      '<div class="card-body card-content danger">' + userInput.notes +' '+ userInput.date +
+      '</div>' +
+      '</div>'
+    );
   });
 });
