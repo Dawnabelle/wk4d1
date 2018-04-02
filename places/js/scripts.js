d62f8c09a6c1170b5245ebda71b1c46c995602b1
Cameron Button, Kelsey Guerra and Matt Nardoni <cameron@>
2018-04-02 13:07:41 -0700

    Front end set up

--- places/js/scripts.js
+++ places/js/scripts.js
@@ -3,8 +3,21 @@ function locationTest (place, time) {
   this.time = time;
 }
 
+function makeCardBody (){
+
+}
+
 var texas = new locationTest('Texas', 'spring 2018');
 
 locationTest.prototype.toString = function locationTestToString() {
   return this.place + ' ' + this.time;
 }
+
+
+$(document).ready(function() {
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    var userInput = new locationTest($("#place").val(), $("#time").val());
+    $("#places-list").append();
+  });
+});
